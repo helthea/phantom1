0b14831981f20dbce456aab9251beec5dd6b654d
Malcolm Diggs <malcolmdiggs@gmail.com>
2017-12-07 15:02:06 +0800

    push timeout into a variable

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ var page = require('webpage').create();
 var url = 'http://s3.amazonaws.com/birdkage/experiments.html';
 page.open(url, function (status) {
   //Page is loaded!
+  var mins = 45;
   console.log('starting render');
   setTimeout(function(){
     console.log('render complete');
     phantom.exit();
-  },1000 * 60 * 60);
-  
+    
+  },1000 * 60 * mins);
+
 });
