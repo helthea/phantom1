2f26f96828fbfe5eb426bca1940d61fa1f062961
Malcolm Diggs <malcolmdiggs@gmail.com>
2017-12-07 14:23:48 +0800

    first pass

---
+++ index.js
@@ -0,0 +1,12 @@
+var path = require('path')
+var childProcess = require('child_process')
+var phantomjs = require('phantomjs-prebuilt')
+var binPath = phantomjs.path
+
+var childArgs = [
+  path.join(__dirname, 'script.js')
+]
+
+childProcess.execFile(binPath, childArgs, function(err, stdout, stderr) {
+  console.log(err,stdout,stderr);
+})
---
+++ script.js
@@ -0,0 +1,12 @@
+console.log('Loading');
+var page = require('webpage').create();
+var url = 'http://s3.amazonaws.com/birdkage/experiments.html';
+page.open(url, function (status) {
+  //Page is loaded!
+  console.log('starting render');
+  setTimeout(function(){
+    console.log('render complete');
+    phantom.exit();
+  },60000);
+
+});
